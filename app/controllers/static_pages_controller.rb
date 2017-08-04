e873aaec5590eebcaad1271e03eb8657050c07a2
activemode <activemode@gmail.com>
2017-08-04 18:44:09 -0500

    updating after editing views

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,16 @@
+class StaticPagesController < ApplicationController
+
+  def setup
+    #@base_title = "Ruby on Rails Tutorial Sample App"
+  end
+
+  def home
+    #:title = "Home | #{@base_title}"
+  end
+
+  def help
+  end
+
+  def about
+  end
+end

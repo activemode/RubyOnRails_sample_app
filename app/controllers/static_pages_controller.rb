fe3b23d73f2d188c2b316e1d75ce28d2b9b5a467
activemode <activemode@gmail.com>
2017-08-13 17:28:42 -0500

    Add user microposts

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,11 +1,13 @@
 class StaticPagesController < ApplicationController
 
   def setup
-    #@base_title = "Ruby on Rails Tutorial Sample App"
   end
 
   def home
-    #:title = "Home | #{@base_title}"
+    if logged_in?
+      @micropost  = current_user.microposts.build
+      @feed_items = current_user.feed.paginate(page: params[:page])
+    end
   end
 
   def help
